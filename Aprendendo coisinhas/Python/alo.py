b561485ed1d406e5b161823d3e9e2c2e0f06716f
Ester <anarafa453@gmail.com>
2022-02-27 19:36:37 -0300

    aprendendo essa coisa

---
+++ Aprendendo coisinhas/Python/alo.py
@@ -0,0 +1,10 @@
+print("Hello, word")
+nota1 = float(input("Digite a primeira nota do aluno:"))
+nota2 = float(input("Digite a segunda nota do aluno:"))
+media = (nota1 + nota2)/2
+notamed = 6.0
+print("A média do aluno é ", media)
+if media >= notamed:
+   print("Aluno está aprovado!")
+else:
+   print("Aluno irá para a prova final")
