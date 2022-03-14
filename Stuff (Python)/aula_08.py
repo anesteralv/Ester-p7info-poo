35962ae5b6ac74a0f601e61c5f27fe9c684b33b6
Yasmim Torres <yasmimtorressoares12@gmail.com>
2022-03-14 14:44:51 -0300

    akjdk

--- Stuff (Python)/aula_08.py
+++ Stuff (Python)/aula_08.py
@@ -20,5 +20,15 @@ x = Complex(3, 4)
 y = Complex(1, 1)
 z = Complex(3, 7)
 
-l_real= [x.r, y.r, z.i]
+l_real= [x.r, y.r, z.r]
 print(l_real)
+
+class triangulo:
+    def __init__(self, base, altura):
+        self.b = base
+        self.a = altura
+        area = (base*altura)/2
+        print(area)
+
+
+x = triangulo(int(input("Digite o valor da base>")), int(input("Digite o valor da altura:")))
