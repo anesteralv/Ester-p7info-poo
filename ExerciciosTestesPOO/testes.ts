9198da3cf04b846ec69c7d5c76c9469aa928e7d3
Yasmim Torres <yasmimtorressoares12@gmail.com>
2022-04-11 17:27:02 -0300

    desgracaa

--- ExerciciosTestesPOO/testes.ts
+++ ExerciciosTestesPOO/testes.ts
@@ -3,3 +3,6 @@ console.log(typeof renan);
 
 let ling = "JavaScript";
 console.log("O tipo da variável 'ling' é:", typeof ling);
+
+let testin, float=3.14;
+console.log("O tipo da variável 'testin' é:", typeof float);
