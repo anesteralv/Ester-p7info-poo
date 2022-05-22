a66c3c0ce53ca8c68281ee5a62b65a33e3e04d01
anesteralv <anarafa453@gmail.com>
2022-05-21 22:01:08 -0300

    Alterado em 21/05/22

---
+++ treinando/aula-tipos.js
@@ -0,0 +1,18 @@
+var x = 10;
+console.log(x);
+var y = 15.7845;
+console.log(typeof y);
+console.log(y.toPrecision(3));
+/////////tipo string
+var firstName = "Ester";
+console.log(firstName.toUpperCase());
+var lastName = "Alves";
+var fullName = (firstName + "" + lastName);
+console.log(fullName);
+//////array
+var i = [1, 2, 3];
+i.push(8);
+console.log(i);
+var n = ["Ester", "Matheus"];
+var amor = (n[0] + " ama " + n[1]);
+console.log(amor);
---
+++ treinando/aula-tipos.ts
@@ -0,0 +1,37 @@
+let x: number = 10
+
+console.log(x)
+
+var y:number = 15.7845
+
+
+console.log(typeof y)
+
+console.log(y.toPrecision(3))
+
+/////////tipo string
+
+
+let firstName: string = "Ester"
+
+console.log(firstName.toUpperCase())
+
+let lastName: string = "Alves"
+
+let fullName: string = (firstName + "" + lastName);
+
+console.log(fullName)
+
+//////array
+
+let i: number[] = [1, 2, 3]
+
+i.push(8)
+
+console.log(i)
+
+const n: string[] = ["Ester", "Matheus"]
+
+let amor = (n[0] + " ama " + n[1])
+
+console.log(amor)
