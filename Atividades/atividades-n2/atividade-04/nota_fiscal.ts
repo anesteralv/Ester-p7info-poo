15cca0411c45bdf9ce4315a525695ca5de0034a8
Ester <anarafa453@gmail.com>
2022-06-23 17:07:04 -0300

    Atualizando atividade

---
+++ Atividades/atividades-n2/atividade-04/nota_fiscal.ts
@@ -0,0 +1,78 @@
+//
+//
+//
+//
+import {Cliente} from "./cliente";
+import {ItemNotaFiscal} from "./item_nota_fiscal";
+
+export class NotaFiscal {
+
+    /* Atributos */
+    id: number;
+    codigo:number; 
+    data: Date;
+    cliente: Cliente;
+    items :  Array<ItemNotaFiscal>;  
+    valorNota : number;
+
+    /* Métodos */
+
+    /* Método Construtor */    
+    constructor (id : number, codigo: number, cliente : Cliente ) {
+        this.id = id;
+        this.codigo = codigo;
+        this.data = new Date(); 
+        this.cliente = cliente; 
+        this.valorNota = 0.0;
+        this.items = new Array<ItemNotaFiscal>();
+
+
+    }
+
+    /* Métodos Acessores */
+    getid(): number {
+        return this.id;
+    }
+
+    getcodigo(): number {
+        return this.codigo;
+    }
+
+    getdata(): Date {
+        return this.data;
+    }
+
+    /* Métodos Modificadores */     
+    setcodigo(codigo: number): void {
+        this.codigo = codigo;
+    }
+
+    setdata(data : Date) : void {
+        this.data = data;
+    }
+
+     
+
+    adicionarItem(item: ItemNotaFiscal) {
+         this.items.push(item);
+    }
+
+    // Percorrer o array items e calcular o valor total da NotaFiscal
+    calcularValorNotaFiscal() : void {
+        let valorCalculado = 0;
+        for(let i = 0; i < this.items.length; i++){
+            let soma = this.items[i].getquantidade()*this.items[i].getvalor()
+            valorCalculado += soma;
+        }
+        console.log("Total: " + valorCalculado);
+        
+    }
+    // Imprimir a NotaFiscal conforme o Layout definido
+    
+    imprimirNotaFiscal(): void {
+
+
+    }
+
+    
+}
